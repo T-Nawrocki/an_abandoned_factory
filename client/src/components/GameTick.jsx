013f2d624218e6d25f3e5a899ded7650af28fcab
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-09-17 20:24:45 +0100

    gametick behaviour improved by changing to setTimeout, but still not perfect (conflict between click and tick)

--- client/src/components/GameTick.jsx
+++ client/src/components/GameTick.jsx
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 
 // Renderless component
 // Handles effects at game-tick interval (primarily addition of resources from autoclickers)
 
 const GameTick = (props) => {
 
-  const partsToAdd = props.partsClickersTier1
+  const partsToAdd = 1 //props.partsClickersTier1
 
-  setInterval(() => {
-    props.addParts(5)
+  setTimeout(() => {
+    if (partsToAdd) props.addParts(partsToAdd);
   }, 1000);
-
   
   return null;
 }
