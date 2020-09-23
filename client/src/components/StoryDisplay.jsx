64bb1d41d030987fbe979288d9e14470fbc7d96c
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-09-23 09:14:10 +0100

    caret added to storydisplay style

--- client/src/components/StoryDisplay.jsx
+++ client/src/components/StoryDisplay.jsx
@@ -6,8 +6,28 @@ const StoryDisplay = (props) => {
   const newText = props.newText;
   const [display, setDisplay] = useState("");
 
+  const type = text => {
+    const typingSpeed = 100;
+
+
+
+    // let i = 0;
+    // while (i < text.length) {
+    //   setDisplay(display + text[i]);
+    //   i++;
+    // }
+
+    // for (let i = 0; i < text.length; i++) {
+    //   const char = text[i];
+    //   console.log('char :>> ', char);
+    //   setTimeout(setDisplay(display + char), typingSpeed)
+    // //   setDisplay(display + char);
+    // }
+  };
+
   useEffect(() => {
     setDisplay(display + newText);
+    // type(newText)
   }, [newText])
 
   return (
