960d2fcf3f4312339418c71326568c44088d2947
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-09-23 17:21:02 +0100

    minor cleanup of useTypeWriter

--- client/src/hooks/useTypewriter.jsx
+++ client/src/hooks/useTypewriter.jsx
@@ -3,20 +3,20 @@ import React, { useState, useRef, useEffect } from 'react';
 const useTypewriter = (message, interval) => {
   const [txt, setTxt] = useState("");
 
-  const intervalRef = useRef();
+  const intervalRef = useRef();  // provides consistent single container for interval
 
   useEffect(() => {
     let i = 0;
     clearInterval(intervalRef.current);  // clear interval if message changes
-    if (!message.length) return; // do nothing if message is empty
+    if (!message.length) return;  // do nothing if message is empty
 
     intervalRef.current = setInterval(() => {
-      setTxt(t => t + message[i]);
+      setTxt(t => t + message[i]);  // callback syntax to ensure use of previous state
       i++;
-      if (i == message.length) clearInterval(intervalRef.current); // clear interval at end of message
+      if (i === message.length) clearInterval(intervalRef.current);  // clear interval at end of message
     }, interval);
 
-    return () => clearInterval(intervalRef.current); // clear interval on unmount
+    return () => clearInterval(intervalRef.current);  // clear interval on unmount
   }, [message, intervalRef, setTxt]);
 
   return [txt];
