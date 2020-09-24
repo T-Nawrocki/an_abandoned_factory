a83a93e62841a02dce65178b16e33333fb0f73f3
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-09-24 10:07:47 +0100

    minor aesthetic touches before presentation

--- client/src/components/StoryDisplay.jsx
+++ client/src/components/StoryDisplay.jsx
@@ -4,10 +4,10 @@ import '../styles/StoryDisplay.scss';
 
 const StoryDisplay = (props) => {
 
-  const [display] = useTypewriter(props.newText, 25);
+  const [display] = useTypewriter(props.newText, 50);
 
   return (
-    <div className="story-display">
+    <div id="main-story-display" className="story-display">
       <div className="story-display-text">
         {display}
       </div>
